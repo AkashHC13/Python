7bc60b033089ecdb0a8a0e0ab5ad5c7f63a2dd5f
AkashHC13 <akashhc@gmail.com>
2018-05-19 19:34:17 +0530

    Simply

---
+++ AboutMe/main.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+print "Hello there. here is a picture of a dog"
+print " o____ "
+print "  |||| "
+print "My name is Akash"
+print "My age is " + str(2017 - 1992)
+print "I live isn a remote place"
+print  "ba" + "na"*2
+print "Here comes the sine wave"
+print "/\  " * 10
+print "  \/" * 10
---
+++ TurtleRace/main.py
@@ -0,0 +1,5 @@
+import turtle
+turtlename = turtle.Turtle()
+wn = turtle.Screen()
+
+turtlename.forward(90)
